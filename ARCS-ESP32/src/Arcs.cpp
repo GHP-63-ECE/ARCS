4f849a687d2dd29483b4d51bbc814d00743d81c8
Spencer Perry <Spencer.e.perry@outlook.com>
2026-06-24 08:36:11 -0400

    Encoder foundations

--- ARCS-ESP32/src/Arcs.cpp
+++ ARCS-ESP32/src/Arcs.cpp
@@ -20,6 +20,10 @@ const int IN7 = 5;
 const int IN8 = 4;
 
 
+// Encoder Connections
+const int ENCA = 2; // Encoder A pin for Motor A
+const int ENCB = 3; // Encoder B pin for Motor B
+
 void setup() {
   // Set all control pins to outputs
   pinMode(ENA, OUTPUT);
@@ -96,3 +100,126 @@ void setSpeed(int speedA, int speedB) {
   analogWrite(ENA, speedA);
   analogWrite(ENB, speedB);
 }
+
+
+/*// Code copied from https://create.arduino.cc/projecthub/curiores/how-to-control-a-dc-motor-with-an-encoder-d1734c
+// for discussion at https://forum.arduino.cc/t/programing-motor-with-encoder/962793/10
+//
+// DaveX added Serial.print(pwr*dir)
+// In Wokwi, there's no functional motor or encoder,
+// so try the Serial Plotter icon to see what it is trying to do
+
+#include <util/atomic.h> // For the ATOMIC_BLOCK macro
+
+#define ENCA 12 // YELLOW
+#define ENCB 13 // WHITE
+#define PWM 3
+#define IN2 4
+#define IN1 5
+
+volatile int posi = 0; // specify posi as volatile: https://www.arduino.cc/reference/en/language/variables/variable-scope-qualifiers/volatile/
+long prevT = 0;
+float eprev = 0;
+float eintegral = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ENCA,INPUT);
+  pinMode(ENCB,INPUT);
+  attachInterrupt(digitalPinToInterrupt(ENCA),readEncoder,RISING);
+
+  pinMode(PWM,OUTPUT);
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+
+  Serial.println("target pos");
+}
+
+void loop() {
+
+  // set target position
+  //int target = 1200;
+  int target = 250*sin(prevT/1e6);
+
+  // PID constants
+  float kp = 1;
+  float kd = 0.025;
+  float ki = 0.0;
+
+  // time difference
+  long currT = micros();
+  float deltaT = ((float) (currT - prevT))/( 1.0e6 );
+  prevT = currT;
+
+  // Read the position in an atomic block to avoid a potential
+  // misread if the interrupt coincides with this code running
+  // see: https://www.arduino.cc/reference/en/language/variables/variable-scope-qualifiers/volatile/
+  int pos = 0;
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+    pos = posi;
+  }
+
+  // error
+  int e = pos - target;
+
+  // derivative
+  float dedt = (e-eprev)/(deltaT);
+
+  // integral
+  eintegral = eintegral + e*deltaT;
+
+  // control signal
+  float u = kp*e + kd*dedt + ki*eintegral;
+
+  // motor power
+  float pwr = fabs(u);
+  if( pwr > 255 ){
+    pwr = 255;
+  }
+
+  // motor direction
+  int dir = 1;
+  if(u<0){
+    dir = -1;
+  }
+
+  // signal the motor
+  setMotor(dir,pwr,PWM,IN1,IN2);
+
+
+  // store previous error
+  eprev = e;
+
+  Serial.print(target);
+  Serial.print(" ");
+  Serial.print(pos);
+  Serial.print(" ");
+  Serial.print(pwr*dir);
+  Serial.println();
+}
+
+void setMotor(int dir, int pwmVal, int pwm, int in1, int in2){
+  analogWrite(pwm,pwmVal);
+  if(dir == 1){
+    digitalWrite(in1,HIGH);
+    digitalWrite(in2,LOW);
+  }
+  else if(dir == -1){
+    digitalWrite(in1,LOW);
+    digitalWrite(in2,HIGH);
+  }
+  else{
+    digitalWrite(in1,LOW);
+    digitalWrite(in2,LOW);
+  }
+}
+
+void readEncoder(){
+  int b = digitalRead(ENCB);
+  if(b > 0){
+    posi++;
+  }
+  else{
+    posi--;
+  }
+}*/
