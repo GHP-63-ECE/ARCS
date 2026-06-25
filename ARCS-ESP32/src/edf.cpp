d0f9716d6028ac29ca4aecf12cc10b288bdefbf9
Charles Kennedy <82charkenn@csdecatur.net>
2026-06-25 11:49:18 -0400

    basic edf code

--- ARCS-ESP32/src/edf.cpp
+++ ARCS-ESP32/src/edf.cpp
@@ -1,11 +1,27 @@
+#include <Servo.h> // ONLY LIBRARY NECESARY FOR ESC 
+#include <Wire.h>
+#include <SPI.h>
 #include <Arduino.h>
 
-const int EDF_PIN = 4; // Pin connected to the EDF motor controller
+byte servoPin = 9; // signal pin for the ESC.
+Servo servo;
+
+
 
 void setup() {
-  pinMode(EDF_PIN, OUTPUT); // Set the EDF pin as an output
+  Serial.begin(115200);
+
+  // ESC ARMING PROCESS //
+  servo.attach(servoPin);
+  servo.writeMicroseconds(1500); // send "stop" signal to ESC. Also necessary to arm the ESC.
+  Serial.println("ESC TEST PREP");
+  delay(7000); // delay to allow the ESC to recognize the stopped signal.
 }
 
+
 void loop() {
-  analogWrite(EDF_PIN, 128); // Set the EDF motor speed (0-255)
+  int pwmVal = map(1500,0, 1023, 1100, 1900); // translate POT values to ESC value.
+  float percentVal = ((pwmVal - 1100) / 8);
+  servo.writeMicroseconds(pwmVal); // Send signal to ESC.
+  delay(50);
 }
