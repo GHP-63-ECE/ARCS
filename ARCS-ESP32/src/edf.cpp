8f15c9342838271749d23aaddd5023d9b02da789
Charles Kennedy <82charkenn@csdecatur.net>
2026-06-27 10:30:28 -0400

    fan works

--- ARCS-ESP32/src/edf.cpp
+++ ARCS-ESP32/src/edf.cpp
@@ -2,25 +2,44 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <Arduino.h>
+#include <BluetoothSerial.h>
 
-byte servoPin = 9; // signal pin for the ESC.
+BluetoothSerial BS;
+
+byte servoPin = 13; // signal pin for the ESC.
 Servo servo;
 
+int powerVal = 1100;
 
 
 void setup() {
   Serial.begin(115200);
+  BS.begin("ARCSEDF");
 
   // ESC ARMING PROCESS //
   servo.attach(servoPin);
   servo.writeMicroseconds(1500); // send "stop" signal to ESC. Also necessary to arm the ESC.
   Serial.println("ESC TEST PREP");
   delay(7000); // delay to allow the ESC to recognize the stopped signal.
+
 }
 
 
 void loop() {
-  int pwmVal = map(1500,0, 1023, 1100, 1900); // translate POT values to ESC value.
+    if(BS.available()){
+        char inc = BS.read();
+        if(inc == '+'){
+            powerVal += 100;
+            BS.println(powerVal);
+        } else if (inc == '-'){
+            powerVal -= 100;
+            BS.println(powerVal);
+        } else if (inc == 'k'){
+            powerVal = 0;
+            BS.println(powerVal);
+        }
+    }
+  int pwmVal = map(powerVal,0, 1023, 1100, 1900); // translate POT values to ESC value.
   float percentVal = ((pwmVal - 1100) / 8);
   servo.writeMicroseconds(pwmVal); // Send signal to ESC.
   delay(50);
