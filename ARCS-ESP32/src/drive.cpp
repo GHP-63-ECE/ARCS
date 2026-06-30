b8d36df75d4ee6c6778ded800a2a4f93dd67ef61
Charles Kennedy <82charkenn@csdecatur.net>
2026-06-29 20:04:58 -0400

    integrated into drive

--- ARCS-ESP32/src/drive.cpp
+++ ARCS-ESP32/src/drive.cpp
@@ -1,308 +1,350 @@
-// #include <Arduino.h>
-// #include <BluetoothSerial.h>
-// #include <ESP32Servo.h> // ONLY LIBRARY NECESARY FOR ESC 
-// #include <Wire.h>
-// #include <SPI.h>
+#include <Arduino.h>
+#include <BluetoothSerial.h>
+#include <ESP32Servo.h> // ONLY LIBRARY NECESARY FOR ESC 
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_MPU6050.h>
 
 
-// BluetoothSerial BS;
-// //#include <HardwareSerial.h>
+BluetoothSerial BS;
 
-// // Function prototypes because c++ is a liar
-// void stopAllMotors();
-// void directionForward();
-// void directionBackward();
-// void setSpeed(int speedA, int speedB);
-// void updateEncoderLeft();
-// void updateEncoderRight();
-// void driveDistance(float distance, int speed);
-// void forwards();
-// void backwards();
-// void left();
-// void right();
+Adafruit_MPU6050 imu;
 
-// //Raspberry Pi communication Pin Definitions
-// //#define RXD2 16  // GPIO16 as RX
-// //#define TXD2 17  // GPIO17 as TX
+float gyroX = 0;
+float gyroY = 0;
+float gyroZ = 0;
 
-// //HardwareSerial mySerial(2); // Use Serial2
+float accelX;
+float accelY;
+float accelZ;
+
+double gyroX_offset = 0, gyroY_offset = 0, gyroZ_offset = 0;
+double accelX_offset = 0, accelY_offset = 0, accelZ_offset = 0;
+double raw_ax, raw_ay, raw_az;
+double raw_gx, raw_gy, raw_gz;
+double gyroX_offset_rad, gyroY_offset_rad, gyroZ_offset_rad;
+
+const int numSamples = 2000; // Number of readings to average
+
+//#include <HardwareSerial.h>
+
+// Function prototypes because c++ is a liar
+void stopAllMotors();
+void directionForward();
+void directionBackward();
+void setSpeed(int speedA, int speedB);
+void updateEncoderLeft();
+void updateEncoderRight();
+void driveDistance(float distance, int speed);
+void forwards();
+void backwards();
+void left();
+void right();
+
+//Raspberry Pi communication Pin Definitions
+//#define RXD2 16  // GPIO16 as RX
+//#define TXD2 17  // GPIO17 as TX
+
+//HardwareSerial mySerial(2); // Use Serial2
  
-// // Motor Pins
-// // Left
-// const int PWML = 23;
-// const int L1 = 22;
-// const int L2 = 21;
+// Motor Pins
+// Left
+const int PWML = 23;
+const int L1 = 22;
+const int L2 = 21;
 
-// // Right
-// const int PWMR = 19;
-// const int R1 = 18;
-// const int R2 = 5;
+// Right
+const int PWMR = 19;
+const int R1 = 18;
+const int R2 = 5;
 
-// byte servoPin = 13; // signal pin for the ESC.
-// Servo servo;
+byte servoPin = 13; // signal pin for the ESC.
+Servo servo;
 
-// // Encoder Connections
-// const int ENCAFL = 34; // Encoder A pin for Front Left Motor
-// const int ENCBFL = 35; // Encoder B pin for Front Left Motor
+// Encoder Connections
+const int ENCAFL = 34; // Encoder A pin for Front Left Motor
+const int ENCBFL = 35; // Encoder B pin for Front Left Motor
 
-// const int ENCAFR = 36; // Encoder A pin for Front Right Motor
-// const int ENCBFR = 39; // Encoder B pin for Front Right Motor
+const int ENCAFR = 36; // Encoder A pin for Front Right Motor
+const int ENCBFR = 39; // Encoder B pin for Front Right Motor
 
-// volatile long encoderValueLeft = 0;
-// volatile long encoderValueRight = 0;
+volatile long encoderValueLeft = 0;
+volatile long encoderValueRight = 0;
 
-// int powerValue = 1100;
+int powerValue = 1100;
 
-// const float wheelDiameter = 44.0; // mm
-// const long ticksPerRotation = 7*298; 
-// const float wheelCircumference = wheelDiameter * PI;
-// const float trackWidth = 150.0; // mm - TODO
-// float movementSpeed = 128.0; // Speed for driving forward/backward (0-255)
-// const float rpmAtMaxSpeed = 100; // Maximum RPM of the motor at full speed - TODO
-// const float turnSpeed = 128.0; // Speed for turning left/right (0-255) - TODO
+const float wheelDiameter = 44.0; // mm
+const long ticksPerRotation = 7*298; 
+const float wheelCircumference = wheelDiameter * PI;
+const float trackWidth = 150.0; // mm - TODO
+float movementSpeed = 128.0; // Speed for driving forward/backward (0-255)
+const float rpmAtMaxSpeed = 100; // Maximum RPM of the motor at full speed - TODO
+const float turnSpeed = 128.0; // Speed for turning left/right (0-255) - TODO
 
-// const float cameraFOVWidthMM = 100.0; // Width of the camera's field of view in millimeters - TODO
-// const float cameraFOVHeightMM = 75.0; // Height of the camera's field of view in millimeters - TODO
+const float cameraFOVWidthMM = 100.0; // Width of the camera's field of view in millimeters - TODO
+const float cameraFOVHeightMM = 75.0; // Height of the camera's field of view in millimeters - TODO
 
-// float RPM(float speed) {
-//   return rpmAtMaxSpeed * (speed / 255.0);
-// }
+float RPM(float speed) {
+  return rpmAtMaxSpeed * (speed / 255.0);
+}
 
-// float mmPerSecond(float speed) {
-//   return (RPM(speed) * wheelCircumference) / 60;
-// }
+float mmPerSecond(float speed) {
+  return (RPM(speed) * wheelCircumference) / 60;
+}
 
-// float degreesPerSecond(float speed) {
-//   return (2 * mmPerSecond(speed) / trackWidth) * 180 / PI;
-// }
+float degreesPerSecond(float speed) {
+  return (2 * mmPerSecond(speed) / trackWidth) * 180 / PI;
+}
 
-// void driveDistanceWithoutEncoders(float distance, int speed) {
-//   float timeToDrive = distance / mmPerSecond(speed);
-//   timeToDrive = abs(timeToDrive); // Ensure time is positive
-//   setSpeed(speed, speed);
-//   delay(timeToDrive * 1000);
-//   stopAllMotors();
-// }
+void driveDistanceWithoutEncoders(float distance, int speed) {
+  float timeToDrive = distance / mmPerSecond(speed);
+  timeToDrive = abs(timeToDrive); // Ensure time is positive
+  setSpeed(speed, speed);
+  delay(timeToDrive * 1000);
+  stopAllMotors();
+}
 
-// void rotateDegreesWithoutEncoders(float degrees, int speed) {
-//   float timeToRotate = degrees / degreesPerSecond(speed);
-//   timeToRotate = abs(timeToRotate); // Ensure time is positive
-//   if (degrees > 0) {
-//     setSpeed(speed, -speed); // Turn right
-//   } else {
-//     setSpeed(-speed, speed); // Turn left
-//   }
-//   delay(timeToRotate * 1000);
-//   stopAllMotors();
-// }
+void rotateDegreesWithoutEncoders(float degrees, int speed) {
+  float timeToRotate = degrees / degreesPerSecond(speed);
+  timeToRotate = abs(timeToRotate); // Ensure time is positive
+  if (degrees > 0) {
+    setSpeed(speed, -speed); // Turn right
+  } else {
+    setSpeed(-speed, speed); // Turn left
+  }
+  delay(timeToRotate * 1000);
+  stopAllMotors();
+}
 
-// // Calculating distance to the center of a crack based on normalized coordinates (cx, cy) of the crack in the camera's field of view
-// float distanceToCrackCenter(float cx, float cy) {
-//   // Convert normalized pixel coordinates to millimeters
-//   float x_mm = (cx - 0.5) * cameraFOVWidthMM;
-//   float y_mm = (cy - 0.5) * cameraFOVHeightMM;
+// Calculating distance to the center of a crack based on normalized coordinates (cx, cy) of the crack in the camera's field of view
+float distanceToCrackCenter(float cx, float cy) {
+  // Convert normalized pixel coordinates to millimeters
+  float x_mm = (cx - 0.5) * cameraFOVWidthMM;
+  float y_mm = (cy - 0.5) * cameraFOVHeightMM;
 
-//   // Calculate distance to the center of the crack using Pythagorean theorem
-//   return sqrt(x_mm * x_mm + y_mm * y_mm);
-// }
+  // Calculate distance to the center of the crack using Pythagorean theorem
+  return sqrt(x_mm * x_mm + y_mm * y_mm);
+}
 
-// float angleToCrackCenter(float cx, float cy) {
-//   // Convert normalized pixel coordinates to millimeters
-//   float x_mm = (cx - 0.5) * cameraFOVWidthMM;
-//   float y_mm = (cy - 0.5) * cameraFOVHeightMM;
+float angleToCrackCenter(float cx, float cy) {
+  // Convert normalized pixel coordinates to millimeters
+  float x_mm = (cx - 0.5) * cameraFOVWidthMM;
+  float y_mm = (cy - 0.5) * cameraFOVHeightMM;
 
-//   // Calculate angle to the center of the crack using arctangent
-//   return atan2(x_mm, y_mm) * (180 / PI); // atan2 is flipped so that 0 degrees is forward and positive angles are to the right
-// }
+  // Calculate angle to the center of the crack using arctangent
+  return atan2(x_mm, y_mm) * (180 / PI); // atan2 is flipped so that 0 degrees is forward and positive angles are to the right
+}
 
-// // Drives to the crack's center such that the crack is centered in the camera's field of view at (0.5, 0.5) 
-// void driveToCrackCenter(float cx, float cy) {
-//   float distance = distanceToCrackCenter(cx, cy);
-//   float angle = angleToCrackCenter(cx, cy);
+// Drives to the crack's center such that the crack is centered in the camera's field of view at (0.5, 0.5) 
+void driveToCrackCenter(float cx, float cy) {
+  float distance = distanceToCrackCenter(cx, cy);
+  float angle = angleToCrackCenter(cx, cy);
 
-//   // Rotate to face the crack center
-//   rotateDegreesWithoutEncoders(angle, turnSpeed);
+  // Rotate to face the crack center
+  rotateDegreesWithoutEncoders(angle, turnSpeed);
 
-//   // Drive forward to the crack center
-//   driveDistanceWithoutEncoders(distance, movementSpeed);
-// }
+  // Drive forward to the crack center
+  driveDistanceWithoutEncoders(distance, movementSpeed);
+}
 
 
-// void setup() {
+void setup() {
 
-//   Serial.begin(115200);
-//   BS.begin("ARCS");
-//   BS.print("Started:");
-//   servo.attach(servoPin);
-//   servo.writeMicroseconds(1500); // send "stop" signal to ESC. Also necessary to arm the ESC.
-//   BS.println("ESC TEST PREP");
-//   delay(7000);
-//    // Initialize Serial Monitor for debugging
-//   //mySerial.begin(115200, SERIAL_8N1, RXD2, TXD2); // Initialize Serial2 with defined pins
-//   //mySerial.println("Wake up from the matrix!");
+  Serial.begin(115200);
+  BS.begin("ARCS");
+  BS.print("Started:");
+  servo.attach(servoPin);
+  servo.writeMicroseconds(1500); // send "stop" signal to ESC. Also necessary to arm the ESC.
+  BS.println("ESC TEST PREP");
+  delay(7000);
+   // Initialize Serial Monitor for debugging
+  //mySerial.begin(115200, SERIAL_8N1, RXD2, TXD2); // Initialize Serial2 with defined pins
+  //mySerial.println("Wake up from the matrix!");
 
-//   // Set all control pins to outputs
+  // Set all control pins to outputs
 
-//   //  Left
-//   pinMode(PWML, OUTPUT);
-//   pinMode(L1, OUTPUT);
-//   pinMode(L2, OUTPUT);
-//   //  Right
-//   pinMode(PWMR, OUTPUT);
-//   pinMode(R1, OUTPUT);
-//   pinMode(R2, OUTPUT);
+  //  Left
+  pinMode(PWML, OUTPUT);
+  pinMode(L1, OUTPUT);
+  pinMode(L2, OUTPUT);
+  //  Right
+  pinMode(PWMR, OUTPUT);
+  pinMode(R1, OUTPUT);
+  pinMode(R2, OUTPUT);
 
-//   // Set encoder pins to interrupts
-//   attachInterrupt(digitalPinToInterrupt(ENCAFL), updateEncoderLeft, RISING);
-//   attachInterrupt(digitalPinToInterrupt(ENCBFL), updateEncoderLeft, RISING);
+      if(!imu.begin()){
+        Serial.print("IMU not found");
+    }
 
-//   attachInterrupt(digitalPinToInterrupt(ENCAFR), updateEncoderRight, RISING);
-//   attachInterrupt(digitalPinToInterrupt(ENCBFR), updateEncoderRight, RISING);
+    imu.setAccelerometerRange(MPU6050_RANGE_2_G);
+    imu.setGyroRange(MPU6050_RANGE_2000_DEG);
+
+  // Set encoder pins to interrupts
+  attachInterrupt(digitalPinToInterrupt(ENCAFL), updateEncoderLeft, RISING);
+  attachInterrupt(digitalPinToInterrupt(ENCBFL), updateEncoderLeft, RISING);
+
+  attachInterrupt(digitalPinToInterrupt(ENCAFR), updateEncoderRight, RISING);
+  attachInterrupt(digitalPinToInterrupt(ENCBFR), updateEncoderRight, RISING);
   
-//   // Turn off motors initially
-//   stopAllMotors();
-// }
+  // Turn off motors initially
+  stopAllMotors();
+}
 
-// void loop() {
+void loop() {
 
-//     if (BS.available() > 0) {
-//     char dataFromPi = BS.read();
-//        switch(dataFromPi) {
-//       case 'w':
-//         forwards();
-//         BS.print('w');
-//         break;
-//       case 's':
-//         backwards();
-//         BS.print('s');
-//         break;
-//       case 'a':
-//         left();
-//         BS.print('a');
-//         break;
-//       case 'd':
-//         right();
-//         BS.print('d');
-//         break;
-//       case ' ':
-//         stopAllMotors();
-//         BS.print("Stop");
-//         break;
+    if (BS.available() > 0) {
+    char dataFromPi = BS.read();
+       switch(dataFromPi) {
+      case 'w':
+        forwards();
+        BS.print('w');
+        break;
+      case 's':
+        backwards();
+        BS.print('s');
+        break;
+      case 'a':
+        left();
+        BS.print('a');
+        break;
+      case 'd':
+        right();
+        BS.print('d');
+        break;
+      case ' ':
+        stopAllMotors();
+        BS.print("Stop");
+        break;
 
-//       case '+':
-//         powerValue += 100;
-//         BS.println(powerValue);
-//         break;
-//       case '-':
-//         powerValue -= 100;
-//         BS.println(powerValue);
-//         break;
-//       case 'k':
-//         powerValue = 0;
-//         BS.println(powerValue);
-//         break;
-//       default:
-//         BS.println("Unknown command received: " + dataFromPi);
-//         break;
-//     }
-//     int pwmVal = map(powerValue,0, 1023, 1100, 1900); // translate POT values to ESC value.
-//     float percentVal = ((pwmVal - 1100) / 8);
-//     servo.writeMicroseconds(pwmVal);
-//     }
-//   }
+      case '+':
+        powerValue += 100;
+        BS.println(powerValue);
+        break;
+      case '-':
+        powerValue -= 100;
+        BS.println(powerValue);
+        break;
+      case 'k':
+        powerValue = 0;
+        BS.println(powerValue);
+        break;
+      default:
+        BS.println("Unknown command received: " + dataFromPi);
+        break;
+    }
+    int pwmVal = map(powerValue,0, 1023, 1100, 1900); // translate POT values to ESC value.
+    float percentVal = ((pwmVal - 1100) / 8);
+    servo.writeMicroseconds(pwmVal);
+    }
 
+    sensors_event_t accel, gyro, temp;
+    imu.getEvent(&accel, &gyro, &temp);
 
-// void forwards() {
-//  setSpeed(movementSpeed, movementSpeed);
-// }
+    accelX = accel.acceleration.x - 0.4;
+    accelY = accel.acceleration.y + 0.1;
+    accelZ = accel.acceleration.z - 0.84;
 
-
-// void backwards() {
-//  setSpeed(-movementSpeed, -movementSpeed);
-// }
-
-
-// void left() {
-//  setSpeed(-movementSpeed, movementSpeed);
-// }
-
-
-// void right() {
-//  setSpeed(movementSpeed, -movementSpeed);
-// }
+    Serial.print("Accel X:");
+    Serial.print(accelX);
+    Serial.print(", Y:");
+    Serial.print(accelY);
+    Serial.print(", Z:");
+    Serial.print(accelZ);
+    Serial.print("m/s^2 ");
+  }
 
 
-// void stopLeftMotors() {
-//   digitalWrite(L1, LOW);
-//   digitalWrite(L2, LOW);
-//   digitalWrite(PWML, 0);
-// }
-
-// void stopRightMotors() {
-//   digitalWrite(R1, LOW);
-//   digitalWrite(R2, LOW);
-//   digitalWrite(PWMR, 0);
-// }
-
-// void stopAllMotors() {
-//   stopLeftMotors();
-//   stopRightMotors();
-// }
-
-// void LeftMotorsForwards() {
-//   digitalWrite(L1, HIGH);
-//   digitalWrite(L2, LOW);
-// }
-
-// void RightMotorsForwards() {
-//   digitalWrite(R1, HIGH);
-//   digitalWrite(R2, LOW);
-// }
-
-// void LeftMotorsBackwards() {
-//   digitalWrite(L1, LOW);
-//   digitalWrite(L2, HIGH);
-// }
-
-// void RightMotorsBackwards() {
-//   digitalWrite(R1, LOW);
-//   digitalWrite(R2, HIGH);
-// }
-
-//  void setSpeed(int left, int right){
-//   if (left == 0) {
-//     stopLeftMotors();
-//   } else if (left < 0) {
-//     LeftMotorsBackwards();
-//     left = -left;
-//   } else {
-//     LeftMotorsForwards();
-//   }
+void forwards() {
+ setSpeed(movementSpeed, movementSpeed);
+}
 
 
-//   if (right == 0) {
-//     stopRightMotors();
-//   } else if (right < 0) {
-//     RightMotorsBackwards();
-//     right = -right;
-//   } else {
-//     RightMotorsForwards();
-//   }
-//   analogWrite(PWML, left);
-//   analogWrite(PWMR, right);
-// }
+void backwards() {
+ setSpeed(-movementSpeed, -movementSpeed);
+}
 
 
-// void updateEncoderLeft(){
-//   if (digitalRead(ENCAFL)> digitalRead(ENCBFL))
-//     encoderValueLeft++;
-//   else
-//     encoderValueLeft--;
-// }
+void left() {
+ setSpeed(-movementSpeed, movementSpeed);
+}
 
-// void updateEncoderRight(){
-//   if (digitalRead(ENCAFR)> digitalRead(ENCBFR))
-//     encoderValueRight++;
-//   else
-//     encoderValueRight--;
-// }
+
+void right() {
+ setSpeed(movementSpeed, -movementSpeed);
+}
+
+
+void stopLeftMotors() {
+  digitalWrite(L1, LOW);
+  digitalWrite(L2, LOW);
+  digitalWrite(PWML, 0);
+}
+
+void stopRightMotors() {
+  digitalWrite(R1, LOW);
+  digitalWrite(R2, LOW);
+  digitalWrite(PWMR, 0);
+}
+
+void stopAllMotors() {
+  stopLeftMotors();
+  stopRightMotors();
+}
+
+void LeftMotorsForwards() {
+  digitalWrite(L1, HIGH);
+  digitalWrite(L2, LOW);
+}
+
+void RightMotorsForwards() {
+  digitalWrite(R1, HIGH);
+  digitalWrite(R2, LOW);
+}
+
+void LeftMotorsBackwards() {
+  digitalWrite(L1, LOW);
+  digitalWrite(L2, HIGH);
+}
+
+void RightMotorsBackwards() {
+  digitalWrite(R1, LOW);
+  digitalWrite(R2, HIGH);
+}
+
+ void setSpeed(int left, int right){
+  if (left == 0) {
+    stopLeftMotors();
+  } else if (left < 0) {
+    LeftMotorsBackwards();
+    left = -left;
+  } else {
+    LeftMotorsForwards();
+  }
+
+
+  if (right == 0) {
+    stopRightMotors();
+  } else if (right < 0) {
+    RightMotorsBackwards();
+    right = -right;
+  } else {
+    RightMotorsForwards();
+  }
+  analogWrite(PWML, left);
+  analogWrite(PWMR, right);
+}
+
+
+void updateEncoderLeft(){
+  if (digitalRead(ENCAFL)> digitalRead(ENCBFL))
+    encoderValueLeft++;
+  else
+    encoderValueLeft--;
+}
+
+void updateEncoderRight(){
+  if (digitalRead(ENCAFR)> digitalRead(ENCBFR))
+    encoderValueRight++;
+  else
+    encoderValueRight--;
+}
