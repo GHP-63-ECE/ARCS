8fb7530d72e9a028cb2e3d167b32fdba323d1be4
Charles Kennedy <82charkenn@csdecatur.net>
2026-07-01 12:00:46 -0400

    testing2

--- ARCS-ESP32/src/extruder.cpp
+++ ARCS-ESP32/src/extruder.cpp
@@ -2,6 +2,9 @@
 #include <PID.h>
 #include <Adafruit_VL6180X.h>
 #include "extruder.h"
+#include <BluetoothSerial.h>
+
+BluetoothSerial BS;
 
 //Gantry pins
 const int ENCODER_PIN_A1 = 0;
@@ -34,6 +37,9 @@ int encoderBValue;
 void setMotorPosition(int targetPos);
 bool isAtTargetPosition();
 void spray();
+void setPowerGantry(int power);
+
+int pw = 100;
 
 bool isFilledFully();
 
@@ -41,10 +47,9 @@ void setup() {
   pinMode(PWM_PIN1, OUTPUT);
   pinMode(PWM_PIN2, OUTPUT);
   pinMode(ENI, OUTPUT);
-  pinMode(ENCODER_PIN_A1, INPUT_PULLUP);
-  pinMode(ENCODER_PIN_B1, INPUT_PULLUP);
 
   Serial.begin(115200);
+  BS.begin("ARCS-Extruder");
 
 //   ir.begin();
 //   depth = ir.readRange();
@@ -62,32 +67,62 @@ void loop() {
     //     motorPosition--;
     // }
 
-    analogWrite(ENI, 100);
-    digitalWrite(PWM_PIN1, LOW);
-    digitalWrite(PWM_PIN2, HIGH);
+    if(BS.available()){
+        char inc = BS.read();
+        if(inc == 'L'){
+            BS.println("Left");
+            setPowerGantry(-pw);
+        } else if (inc == 'R'){
+            BS.println("Right");
+            setPowerGantry(pw);
+        } else if (inc == 'S'){
+            setPowerGantry(0);
+        } else if (inc == '+'){
+            pw += 10;
+            pw = min(pw, 255);
+            BS.println(pw);
+        } else if (inc == '-'){
+            pw -= 10;
+            pw = max(pw, 0);
+            BS.println(pw);
+        }
+    }
     // depth = ir.readRange();
 }
 
-void setMotorPosition(int targetPos) {
-    int error = targetPos - motorPosition;
-    targetPosition = targetPos;
+// void setMotorPosition(int targetPos) {
+//     int error = targetPos - motorPosition;
+//     targetPosition = targetPos;
 
-    pidController.UpdateError(error);
+//     pidController.UpdateError(error);
 
-    analogWrite(PWM_PIN1, pidController.p_error*kP + pidController.i_error*kI + pidController.d_error*kD);    
-}
-
-bool isAtTargetPosition() {
-    return abs(targetPosition - motorPosition) < 2;
-}
+//     analogWrite(PWM_PIN1, pidController.p_error*kP + pidController.i_error*kI + pidController.d_error*kD);    
+// }
 
 void spray(){
     analogWrite(ENI, 100);
     digitalWrite(PWM_PIN1, LOW);
     digitalWrite(PWM_PIN2, HIGH);
+    Serial.println("Spraying");
 
 }
 
-bool isFilledFully(){
-    return abs(STANDARD_DISTANCE - depth) < 4;
+void setPowerGantry(int power) {
+    
+    analogWrite(ENI, power);
+
+    if(power == 0) {
+        digitalWrite(PWM_PIN1, LOW);
+        digitalWrite(PWM_PIN2, LOW);
+        analogWrite(ENI, 0);
+        return;
+    } else if (power < 0) {
+        power = -power;
+        digitalWrite(PWM_PIN1, HIGH);
+        digitalWrite(PWM_PIN2, LOW);
+    } else {
+        digitalWrite(PWM_PIN1, LOW);
+        digitalWrite(PWM_PIN2, HIGH);
+    }
+
 }
